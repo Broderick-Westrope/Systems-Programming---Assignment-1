c18330b287ba47c4380ffc43543176ac14b88cc1
AsclepiiusUnknown <55474351+AsclepiiusUnknown@users.noreply.github.com>
2021-05-02 19:20:48 +1000

    Basic - Working

--- SortList.h
+++ SortList.h
@@ -4,69 +4,29 @@
 
 #ifndef ASSIGNMENT1_SORTLIST_H
 #define ASSIGNMENT1_SORTLIST_H
-
-void swap(IORB *b, IORB *c, IORB *cParent)
+IORB * swap(IORB *ptr1, IORB *ptr2)
 {
-    IORB *temp = c->link;
-    c->link = b->link;
-    b->link = temp;
-    if (cParent != NULL) //null if we are switching for the head because the head has no parent
-        cParent->link = b;
+    IORB *temp = ptr2->link;
+    ptr2->link = ptr1;
+    ptr1->link = temp;
+    return ptr2;
+
 }
 
-IORB *getParent(IORB *head, IORB *child)
+void sortList(IORB** head, int(*prio)(int))
 {
-    IORB *current = head;
-    while (current->link != NULL)
-    {
-        if (current->link == child)
-            return current;
-
-        current = current->link;
+    IORB** current;
+    for (int i = 0; i <= 10; i++) {
+        current = head;
+        for (int j = 0; j < 10 - i - 1; j++) {
+            IORB* ptr1 = *current;
+            IORB* ptr2 = ptr1->link;
+            if (prio(ptr1->base_pri) > prio(ptr2->base_pri)) {
+                *current = swap(ptr1, ptr2);
+            }
+            current = &(*current)->link;
+        }
     }
-
-    return NULL;
-}
-
-
-//Function to display all the elements of a linked list given the head node
-void disp(IORB *head, int(*prio)(int))
-{
-    IORB *temp = head; //Create a temporary variable that points to the head node
-    printf("\n"); //Add a line break
-    int request = 0; //Set the request to 0 (this allows us to display what index each i/o request is
-    while (temp != NULL) //While the temp isn't NULL (ie. while there are still nodes left)
-    {
-        //Print the information for this node including the request index, base priority, and sorting priority
-        printf("Block description: this is i/o request %d Base Priority: %d Priority: %d\n", request, temp->base_pri, prio(temp->base_pri));
-        temp = temp->link; //Update temp to be the next node
-        request++; //Increment request to count the number of i/o requests
-    }
-}
-
-IORB *sortList(IORB *head, int(*prio)(int))
-{
-    disp(head, prio);
-
-    if (head->link == NULL)
-        return head;
-
-    IORB *current = head;
-    IORB *best = head;
-    while (current != NULL)
-    {
-        if (current->base_pri < best->base_pri)
-            best = current;
-        current = current->link;
-    }
-
-    IORB *bestParent = getParent(head, best);
-    if (bestParent == NULL)
-        printf("no parent for node\n");
-    swap(head, best, bestParent);
-    head->link = sortList(head->link, prio);
-
-    return head;
 }
 
 #endif //ASSIGNMENT1_SORTLIST_H
--- main.c
+++ main.c
@@ -1,113 +1,75 @@
-//
-// Created by Broderick on 19/04/2021.
-//
-
-//region - References
 #include <stdio.h>
-#include "stdlib.h"
-#include "time.h"
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
 #include "IORB.h"
 #include "SortList.h"
 #include "PriComp.h"
-//endregion
 
-//region - Prototypes
-void DisplayList(IORB *head, int (*prio)(int));
 
-IORB *BuildList(int size);
-//endregion
+IORB *buildList(int size, int num);
+
+void displayList(IORB *head, int(*prio)(int));
 
 int main()
 {
-    srand(time(NULL)); //Seed the random function for use later (when generating the base priority)
-
-    int done = 0; //A variable used to check if we are done the program or not (continues when it equals 0)
-    IORB *head; //A pointer to the head of the list
-    head->link = NULL; //Set the pointer to NULL as there is no list yet
-    while (done == 0) //While we haven't finished using the program, do the following
+    srand(time(NULL));
+    int done = 0;
+    IORB *list;
+    do
     {
-        int choice = 0; //By default, our choice is 0 (Exit)
+        printf("Welcome to disk I/O handler, please choose an option:\n"
+               "0 - Exit\n"
+               "1 - Build List\n"
+               "2 - Sort List (ascending)\n"
+               "3 - Display List\n");
+        scanf("%d", &done);
+        while (done < 0 || done > 3)
+            scanf("%d", &done);
+        printf("Your choice: %d\n", done);
 
-        //Ask the user what they would like to do
-        printf("\nPlease enter your choice: \n");
-        printf("0) Exit \n"
-               "1) Build List \n"
-               "2) Sort List (ascending) \n"
-               "3) Display List \n"
-               "Your choice: ");
-        //Get the user's input for what to do
-        scanf("%d", &choice);
-
-        //Using the user's input, do the corresponding action
-        switch (choice)
+        if (done == 1)
         {
-            case 0:     //Exit the program
-            {
-                printf("Terminating...");
-                done = 1; //Mark that we are done with the program (stops looping)
-                break;
-            }
-            case 1:     //Build a new list of size 10
-            {
-                head = BuildList(10); //Run the build function
-                break;
-            }
-            case 2:     //Sort the list
-            {
-                if (head->link == NULL) //Debug/Check if the list is empty. If so, tell the user and don't sort
-                {
-                    printf("Cannot sort an empty list!\n");
-                    break;
-                }
-                head = sortList(&head, priComp); //Run the sort function
-                printf("Sorting Complete.\n");
-                break;
-            }
-            case 3:     //Display the list
-            {
-                DisplayList(head, priComp); //Run the display function
-                break;
-            }
-            default:    //If the user has provided invalid input (ie. not 0-3) then exit the program
-            {
-                printf("Invalid input so the program will terminate now.");
-                done = 1;
-            }
+            list = buildList(10, 0);
         }
-        printf("\n\n"); //Add gaps
-    }
+        else if (done == 2)
+        {
+            sortList(&list, priComp);
+        }
+        else if (done == 3)
+            displayList(list, priComp);
+    } while (done != 0);
+
+    printf("Program terminated\n");
     return 0;
 }
 
-//Recursive function to build a list of nodes of a given size
-IORB *BuildList(int size)
-{
-    IORB *this = ((IORB *) malloc(sizeof(IORB))); //Allocate the memory to be used for this node
-    this->base_pri = GenerateBase();
-    if (size <= 1) //If we have reached the last node to be made
-    {
-        this->link = NULL; //Set its link to be NULL (nothing)
-    }
-    else //Else if we are still going
-    {
-        //Recursively call BuildList to make another node and set it as the link of this node
-        this->link = (struct iorb *) BuildList(size - 1);
-    }
 
-    return this; //Return this node we have just made
+IORB *buildList(int size, int num)
+{
+    IORB *node = ((IORB *) malloc(sizeof(IORB)));
+    node->base_pri = GenerateBase();
+    char newFiller[25];
+
+    snprintf(newFiller, 25, "This is i/o request %d ", num);
+    strcpy(node->filler, newFiller);
+    if (size == 0)
+    {
+        node->link = NULL;
+    }
+    else node->link = buildList(size - 1, num + 1);
+
+    return node;
 }
 
-//Function to display all the elements of a linked list given the head node
-void DisplayList(IORB *head, int(*prio)(int))
+void displayList(IORB *head, int(*prio)(int))
 {
-    IORB *temp = head; //Create a temporary variable that points to the head node
-    printf("\n"); //Add a line break
-    int request = 0; //Set the request to 0 (this allows us to display what index each i/o request is
-    while (temp != NULL) //While the temp isn't NULL (ie. while there are still nodes left)
-    {
-        //Print the information for this node including the request index, base priority, and sorting priority
-        printf("Block description: this is i/o request %d Base Priority: %d Priority: %d\n", request, temp->base_pri, prio(temp->base_pri));
-        temp = temp->link; //Update temp to be the next node
-        request++; //Increment request to count the number of i/o requests
-    }
+
+    if (head->link == NULL)
+        return;
+
+    printf("%s Base_Priority: %d Priority: %d\n", head->filler, head->base_pri, priComp(head->base_pri));
+    head = (IORB *) head->link;
+    displayList(head, priComp);
+
 }
