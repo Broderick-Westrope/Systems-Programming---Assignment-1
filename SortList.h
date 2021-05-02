3bd844681ed66b06c91a5f204aba0eae2f3628a4
AsclepiiusUnknown <55474351+AsclepiiusUnknown@users.noreply.github.com>
2021-05-02 20:07:16 +1000

    Recursion attempt

--- SortList.h
+++ SortList.h
@@ -4,7 +4,8 @@
 
 #ifndef ASSIGNMENT1_SORTLIST_H
 #define ASSIGNMENT1_SORTLIST_H
-IORB * swap(IORB *ptr1, IORB *ptr2)
+
+IORB *swap(IORB *ptr1, IORB *ptr2)
 {
     IORB *temp = ptr2->link;
     ptr2->link = ptr1;
@@ -13,20 +14,41 @@ IORB * swap(IORB *ptr1, IORB *ptr2)
 
 }
 
-void sortList(IORB** head, int(*prio)(int))
+void sortList(IORB **head, int(*prio)(int))
 {
-    IORB** current;
-    for (int i = 0; i <= 10; i++) {
+    IORB **current;
+    for (int i = 0; i <= 10; i++)
+    {
         current = head;
-        for (int j = 0; j < 10 - i - 1; j++) {
-            IORB* ptr1 = *current;
-            IORB* ptr2 = ptr1->link;
-            if (prio(ptr1->base_pri) > prio(ptr2->base_pri)) {
+        for (int j = 0; j < 10 - i - 1; j++)
+        {
+            IORB *ptr1 = *current;
+            IORB *ptr2 = ptr1->link;
+            if (prio(ptr1->base_pri) > prio(ptr2->base_pri))
+            {
                 *current = swap(ptr1, ptr2);
             }
             current = &(*current)->link;
         }
     }
+}
+
+IORB *rbubble(IORB *head, int (*prio)(int))
+{
+    if (head->link == NULL)
+        return head;
+
+    IORB *current = head;
+    while (current != NULL)
+    {
+        if (current->base_pri > current->link->base_pri)
+            swap(current, current->link);
+        else
+            current = current->link;
+    }
+
+    rbubble(head->link, prio);
+    return head;
 }
 
 #endif //ASSIGNMENT1_SORTLIST_H
--- main.c
+++ main.c
@@ -7,7 +7,7 @@
 #include "PriComp.h"
 
 
-IORB *buildList(int size, int num);
+IORB *buildList(int size);
 
 void displayList(IORB *head, int(*prio)(int));
 
@@ -15,61 +15,96 @@ int main()
 {
     srand(time(NULL));
     int done = 0;
-    IORB *list;
-    do
-    {
-        printf("Welcome to disk I/O handler, please choose an option:\n"
-               "0 - Exit\n"
-               "1 - Build List\n"
-               "2 - Sort List (ascending)\n"
-               "3 - Display List\n");
-        scanf("%d", &done);
-        while (done < 0 || done > 3)
-            scanf("%d", &done);
-        printf("Your choice: %d\n", done);
+    IORB *head;
 
-        if (done == 1)
+    while (done == 0)
+    {
+        int choice = 0; //By default, our choice is 0 (Exit)
+
+        //Ask the user what they would like to do
+        printf("\nPlease enter your choice: \n");
+        printf("0) Exit \n"
+               "1) Build List \n"
+               "2) Sort List (ascending) \n"
+               "3) Display List \n"
+               "Your choice: ");
+        //Get the user's input for what to do
+        scanf("%d", &choice);
+
+        //Using the user's input, do the corresponding action
+        switch (choice)
         {
-            list = buildList(10, 0);
+            case 0:     //Exit the program
+            {
+                printf("Terminating...");
+                done = 1; //Mark that we are done with the program (stops looping)
+                break;
+            }
+            case 1:     //Build a new list of size 10
+            {
+                head = buildList(10); //Run the build function
+                break;
+            }
+            case 2:     //Sort the list
+            {
+                if (head->link == NULL) //Debug/Check if the list is empty. If so, tell the user and don't sort
+                {
+                    printf("Cannot sort an empty list!\n");
+                    break;
+                }
+                sortList(&head, priComp); //Run the sort function
+//                rbubble(&head, priComp);
+                printf("Sorting Complete.\n");
+                break;
+            }
+            case 3:     //Display the list
+            {
+                displayList(head, priComp); //Run the display function
+                break;
+            }
+            default:    //If the user has provided invalid input (ie. not 0-3) then exit the program
+            {
+                printf("Invalid input so the program will terminate now.");
+                done = 1;
+            }
         }
-        else if (done == 2)
-        {
-            sortList(&list, priComp);
-        }
-        else if (done == 3)
-            displayList(list, priComp);
-    } while (done != 0);
+        printf("\n\n"); //Add gaps
+    }
 
     printf("Program terminated\n");
     return 0;
 }
 
 
-IORB *buildList(int size, int num)
+//Recursive function to build a list of nodes of a given size
+IORB *buildList(int size)
 {
-    IORB *node = ((IORB *) malloc(sizeof(IORB)));
-    node->base_pri = GenerateBase();
-    char newFiller[25];
+    IORB *this = ((IORB *) malloc(sizeof(IORB))); //Allocate the memory to be used for this node
+    this->base_pri = GenerateBase();
 
-    snprintf(newFiller, 25, "This is i/o request %d ", num);
-    strcpy(node->filler, newFiller);
-    if (size == 0)
+    char fill[25];
+    snprintf(fill, 25, "This is i/o request %d ", (10 - size));
+    strcpy(this->filler, fill);
+
+    if (size <= 1) //If we have reached the last node to be made
     {
-        node->link = NULL;
+        this->link = NULL; //Set its link to be NULL (nothing)
     }
-    else node->link = buildList(size - 1, num + 1);
+    else //Else if we are still going
+    {
+        //Recursively call BuildList to make another node and set it as the link of this node
+        this->link = (struct iorb *) buildList(size - 1);
+    }
 
-    return node;
+    return this; //Return this node we have just made
 }
 
 void displayList(IORB *head, int(*prio)(int))
 {
-
     if (head->link == NULL)
         return;
 
-    printf("%s Base_Priority: %d Priority: %d\n", head->filler, head->base_pri, priComp(head->base_pri));
+    printf("%s Base Priority: %d Priority: %d\n", head->filler, head->base_pri, prio(head->base_pri));
     head = (IORB *) head->link;
     displayList(head, priComp);
-
 }
